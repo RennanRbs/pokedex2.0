0469416ab22a4c11269cbacda4a09990b75720c7
Rennan Rebouças <rennanrbs@icloud.com>
2019-07-04 20:14:14 -0300

    Initial Commit

---
+++ Pokedex_2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex_2.0
+//
+//  Created by Rennan Rebouças on 04/07/19.
+//  Copyright © 2019 Rennan Rebouças. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pokedex_2.0Tests/Pokedex_2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Pokedex_2_0Tests.swift
+//  Pokedex_2.0Tests
+//
+//  Created by Rennan Rebouças on 04/07/19.
+//  Copyright © 2019 Rennan Rebouças. All rights reserved.
+//
+
+import XCTest
+@testable import Pokedex_2_0
+
+class Pokedex_2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
